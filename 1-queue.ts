b0422219a1c3303cd1b2c6443b61380769c38db8
Richard Olpindo <djricky088@yahoo.com>
2025-02-15 13:42:23 -1000

    Completed, implement, isEmpty, size, and print methods

--- 1-queue.ts
+++ 1-queue.ts
@@ -3,12 +3,17 @@
 // - Look into the private keyword. Make sure we can initialize a queue of any input type.
 // - Initialize the queue in the constructor. Remember, we may need to utilize other data structures here to create the behavior we are looking for.
 
-class Queue {
+class Queue<input> {
   private items: any[];
 
-  constructor(item: any) {
+  constructor() {
     this.items = [];
   }
+
+  //   getQueue():any[]{
+  //     return this.items;
+  //   }
+  //   getQueue();
 
   // Step 2: Implement enqueue method
   // - Create a method to add an element to the queue.
@@ -35,20 +40,40 @@ class Queue {
   // - Create a method to return the first element without removing it.
   // - If the queue is empty, return null.
 
+  implement(): any | null {
+    if (this.items.length === 0) {
+      return null;
+    } else {
+      return this.items[0];
+    }
+  }
+
   // Step 5: Implement isEmpty and size methods
   // - Create a method to check if the queue is empty.
   // - Create a method to return the number of elements in the queue.
 
+  isEmpty(): boolean {
+    return this.items.length === 0;
+  }
+
+  size(): number {
+    return this.items.length;
+  }
+
   // Step 6: Implement print method
   // - Create a method to display the queue elements.
   // - Print elements in order, separated by "<-".
+
+  print() {
+    console.log(this.items.join(","));
+  }
 }
 // Uncomment The Code Below to See If It Works! Feel free to write more code to test and examine the functionality of the queue.
-// const queue = new Queue<number>(); // Create a queue that stores numbers
-// queue.enqueue(10);
-// queue.enqueue(20);
-// queue.enqueue(30);
-// queue.print(); // Output: 10 <- 20 <- 30
+const queue = new Queue<number>(); // Create a queue that stores numbers
+queue.enqueue(10);
+queue.enqueue(20);
+queue.enqueue(30);
+queue.print(); // Output: 10 <- 20 <- 30
 // console.log(queue.dequeue()); // 10
 // console.log(queue.front()); // 20
 // console.log(queue.size()); // 2
